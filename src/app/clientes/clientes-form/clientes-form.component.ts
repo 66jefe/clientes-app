a3df1eed5847cd8b0e00be28eef70c68c9910797
Jeferson Prestes <jeferson_prestes@outlook.com>
2022-02-03 23:36:18 -0300

    criado botão de salvar, campos de cadastros

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cliente } from '../cliente';
+
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesFormComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente;
+
+  constructor() {
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
+  }
+
+  clicar(){
+    console.log(this.cliente);
   }
 
 }
