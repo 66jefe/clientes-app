db1f07d23d32c406de289a08c5a5c2d71df6e105
Jeferson Prestes <jeferson_prestes@outlook.com>
2022-02-09 20:48:42 -0300

    Criado botão deletar clientes e adicionado mensagens de erro e sucesso

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router'
 
 import { Cliente } from '../cliente';
+import {ClientesService} from '../../clientes.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
@@ -10,16 +13,55 @@ import { Cliente } from '../cliente';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
+  id: number;
 
-  constructor() {
+  constructor(
+    private service: ClientesService, 
+    private router: Router, 
+    private activatedRoute: ActivatedRoute
+    ) {
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+    let params : Observable<Params> = this.activatedRoute.params;
+    params.subscribe(urlParams =>{
+        this.id = urlParams['id'];
+        if(this.id){
+        this.service.getClienteById(this.id)
+        .subscribe(response => this.cliente = response, 
+          errorResponse => this.cliente = new Cliente());
+        }
+      }
+    )
   }
 
-  clicar(){
-    console.log(this.cliente);
+  onSubmit(){
+    if(this.id){
+      this.service
+      .atualizar(this.cliente)
+      .subscribe(response =>{
+        this.success = true;
+        this.errors = null;
+      }, errorResponse => {
+        this.errors = ['Erro ao atualizar o cliente.']
+      })
+    }else{
+      this.service.salvar(this.cliente).subscribe(response =>{
+        this.success = true;
+        this.errors = null;
+        this.cliente = response;
+      }, errorResponse =>{
+        this.errors = errorResponse.error.errors;
+      });
+
+    }
+  }
+
+  voltarParaListagem(){
+    this.router.navigate(['/clientes-lista']);
   }
 
 }
